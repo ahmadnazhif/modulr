f44ac02f1f1997e2a696fdd3afe601bdb18512dc
Nazhif <64678011+ahmadnazhif@users.noreply.github.com>
2025-05-10 18:45:38 +0700

    Add CRUD Product and Role Based Permission

--- modulr/urls.py
+++ modulr/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('modular_engine.urls')),
-    path('', include('product_module.urls')),
+    path('module/product/', include('product_module.urls')),
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .forms import ProductForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseForbidden
+
 
 def product_list(request):
     products = Product.objects.all()
@@ -8,3 +12,35 @@ def product_list(request):
 def product_detail(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'product_module/product_detail.html', {'product': product})
+
+@login_required
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'product_module/product_form.html', {'form': form})
+
+@login_required
+def product_update(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'product_module/product_form.html', {'form': form})
+
+@login_required
+@permission_required('product_module.delete_product', raise_exception=True)
+def product_delete(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'product_module/product_confirm_delete.html', {'product': product})
