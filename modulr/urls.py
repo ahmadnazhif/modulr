8836dcd3aa850be272b5384b09ce4cb386b30ac9
Nazhif <64678011+ahmadnazhif@users.noreply.github.com>
2025-05-12 10:05:38 +0700

    Redirect Home to Module Page

--- modulr/urls.py
+++ modulr/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', lambda req: redirect('/module/')),
     path('auth/login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('auth/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
